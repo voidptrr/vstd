d64e89ee6c39b3c6377d348e50634888d7db6f58
voidptrr <bruno.tommaso@protonmail.com>
2026-05-12 17:30:41 +0100

    docs: initial setup

---
+++ .github/README.md
@@ -0,0 +1,14 @@
+# Data Structures and Algorithms
+
+This repository contains C implementations of core data structures and algorithms.
+
+## Data Structure Status
+
+| Data Structure | Implemented |
+| --- | --- |
+| Vector | Yes |
+| Queue | No |
+| Linked List | No |
+| Hash Table | No |
+| Binary Search Tree | No |
+| Heap | No |
--- src/vector.h
+++ src/vector.h
@@ -5,18 +5,23 @@
 
 enum vector_status { VECTOR_OK, VECTOR_ERR_OOM, VECTOR_ERR_NULL, VECTOR_ERR_EMPTY };
 
+/*
+ * Generic contiguous dynamic array storage.
+ * +----------------------+----------------------+----------------------+----------------------+
+ * | size (size_t)        | elem_size (size_t)   | capacity (size_t)    | buffer (void *)      |
+ * +----------------------+----------------------+----------------------+----------------------+
+ */
 struct vector {
-    void *buffer;
     size_t size;
     size_t elem_size;
     size_t capacity;
+    void *buffer;
 };
 
 enum vector_status vector_init(struct vector *vector, size_t elem_size);
 enum vector_status vector_push(struct vector *vector, const void *element);
 enum vector_status vector_pop(struct vector *vector, void *out);
 enum vector_status vector_free(struct vector *vector);
-
 size_t vector_size(struct vector *vector);
 
 #endif
